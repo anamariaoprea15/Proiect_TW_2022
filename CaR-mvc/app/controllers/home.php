79b2796dc282eb9d8b6d7cb1e9792c662d5eca03
Oprea Ana-Maria <anamariaoprea15@gmail.com>
2022-06-21 00:47:09 +0300

    settings change password

--- CaR-mvc/app/controllers/home.php
+++ CaR-mvc/app/controllers/home.php
@@ -7,9 +7,6 @@ class Home extends Controller
 
     public function index($name = '')
     {
-        // $user = $this->model('User');
-        // $user->name = $name;
-
         $this->view('home/index');
     }
 
